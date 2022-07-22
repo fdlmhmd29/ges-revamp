84e21a9fc1aca42222517aea38f68eafb2a5f23b
fdlmhmd29 <fadhilmhmd29@gmail.com>
2022-07-22 23:14:54 +0700

    Test update

--- next.config.js
+++ next.config.js
@@ -2,14 +2,15 @@ const nextTranslate = require("next-translate");
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
-module.exports = nextTranslate();
-withMDX({
-  pageExtensions: ["js", "jsx", "md", "mdx"],
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages (mdx) that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
-});
+module.exports = nextTranslate(
+  withMDX({
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages (mdx) that depend on `fs` module
+      if (!isServer) {
+        config.resolve.fallback.fs = false;
+      }
+      return config;
+    },
+  })
+);
